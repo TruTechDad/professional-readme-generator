68fc35e351e093e404e16132fc3a11cc7a51b84f
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-12-26 23:31:55 -0500

    stopping point will resume later

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'Please enter the title of your project:',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please enter a brief description of your project:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please enter installation instructions:',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Please enter usage information:',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause'],
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Please enter contribution guidelines:',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Please enter test instructions:',
+},
+{
+    type: 'input',
+    name: 'username',
+    message: 'Please enter your GitHub username:',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address:',
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
