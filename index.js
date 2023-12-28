757e08b716d7783350441902db0e5b2f190a4ad3
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-12-28 10:43:52 -0500

    added the function for read me file and for initialize app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown'); 
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -64,7 +65,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        writeToFile("README2.md", generateMarkdown(data))
+    });
+}
 
 // Function call to initialize app
 init();
