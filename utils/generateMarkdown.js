cdaae161869b1071c7d0a68abcb6a21606b6d2e7
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-12-29 10:36:19 -0500

    Stopping point

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License](https://img.shields.io/badge/license-${license}-brightgreen.svg)](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `[${license} License](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the terms of the ${renderLicenseLink(license)}.
+
+`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${renderLicenseLink(data.license) ? "- [License](#license)\n" : ''}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions about the project, please contact [${data.username}](https://github.com/${data.username}) via GitHub or [email](mailto:${data.email}).
+
+${renderLicenseSection(data.license)}
 `;
 }
 
